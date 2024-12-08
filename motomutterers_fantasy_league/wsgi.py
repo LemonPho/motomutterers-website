c3ea517fcc23606f3460ce8e613f0eada226f72c
BenitoCamelo2 <louismatthew1112@gmail.com>
2024-12-08 00:21:55 -0600

    adding motomutterers_fantasy_league again

---
+++ motomutterers_fantasy_league/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for motomutters_fantasy_league project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motomutterers_fantasy_league.settings')
+
+application = get_wsgi_application()
