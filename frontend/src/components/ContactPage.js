8c217a3c89753911b62fb8fdbc87532af237cf58
LemonPho <louismatthew1112@gmail.com>
2026-04-01 21:33:01 -0600

    updated contact page

--- frontend/src/components/ContactPage.js
+++ frontend/src/components/ContactPage.js
@@ -2,20 +2,23 @@ import React from "react";
 
 export default function Contact(){
     return(
-        <div className="card my-2 rounded-15 element-background-color element-border-color">
-            <div className="card-header">
+        <div className="card rounded-15 align-middle element-background-color element-border-color p-2">
+            <div className="rounded-15-top card-header rounded-15 nested-element-color mb-3">
                 <h5>Contact us</h5>
             </div>
 
-            <div className="card-body">
-                <span>Feel free to send an email of an issue, feedback is welcomed</span>
-                <p></p>
-                <span><strong>Email:</strong> motomutterersfantasyleague@gmail.com</span>
-                <hr />
-                <span>Donations:</span>
-                <p></p>
-                <span><strong>Paypal link: </strong><a href="https://www.paypal.com/donate/?hosted_button_id=LCXBRSW5TECUQ">https://www.paypal.com/donate/?hosted_button_id=LCXBRSW5TECUQ</a></span>
-            </div>
+            <div className="card-body p-0">
+                <div className="nested-element-color p-2 rounded-15">
+                    <span>Feel free to send an email of an issue, feedback is welcomed</span>
+                    <p></p>
+                    <span><strong>Email:</strong> motomutterersfantasyleague@gmail.com</span>
+                    <hr />
+                    <span>Donations:</span>
+                    <p></p>
+                    <span><strong>Paypal link: </strong><a href="https://www.paypal.com/donate/?hosted_button_id=LCXBRSW5TECUQ">https://www.paypal.com/donate/?hosted_button_id=LCXBRSW5TECUQ</a></span>
+   
+                </div>
+               </div>
         </div>
     )
-};
+};         
