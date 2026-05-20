7e96eb1567feac99f90ce86781885e1a6f551bf4
LemonPho <louismatthew1112@gmail.com>
2026-05-20 08:29:38 -0600

    restore

--- api/views/users_view/user_view.py
+++ api/views/users_view/user_view.py
@@ -320,7 +320,6 @@ def toggle_email_notifications(request):
 
 
 def email_new_password(request):
-    print(request)
     if request.method != "POST":
         return HttpResponse(status=405)
     
@@ -329,14 +328,12 @@ def email_new_password(request):
     User = get_user_model()
 
     if not username:
-        print(username)
         return HttpResponse(status=405)
     
     try:
         user = User.objects.get(username=username)
         email = user.email
     except User.DoesNotExist:
-        print("User not found")
         return HttpResponse(status=405)
     
     domain = get_current_site(request).domain
